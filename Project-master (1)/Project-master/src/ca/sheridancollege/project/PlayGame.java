d065e96343bf1c0ee0fcbdfe86e1f18a9eabb45e
syefaraz <77973094+syefaraz@users.noreply.github.com>
2024-12-04 18:35:13 -0500

    Delete Project-master (1)/Project-master directory

--- Project-master (1)/Project-master/src/ca/sheridancollege/project/PlayGame.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-public class PlayGame {
-
-    public static void main(String[] args) {
-        Blackjack game = new Blackjack();
-        game.play();
-    }
-}
